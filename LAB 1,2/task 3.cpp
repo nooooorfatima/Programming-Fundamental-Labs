0bab4e283f111788460df80677cdd40797b2152f
nooooorfatima <noorfatimakiani5@gmail.com>
2025-12-14 07:32:32 -0800

    basic

---
+++ LAB 1,2/task 3.cpp
@@ -0,0 +1,9 @@
+ #include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"\n item   \t price \t quality"<<endl;
+	cout<<"\n pen    \t $1    \t 10"<<endl;
+	cout<<"\n pencil \t $2    \t 5"<<endl;
+	cout<<"\n eraser \t $0.2  \t 20"<<endl;
+}
---
+++ LAB 1,2/task 5.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"welcome to the c++ lab!"<<endl;
+	cout<<"you will learn basic output today."<<endl;
+	cout<<"let,s start with the simple messages."<<endl;
+}
